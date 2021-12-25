741cc5c58366bc7977bb8c68648f8caad537145a
adib sadeghi <adibsadeghi7272@gmail.com>
2021-12-25 18:22:47 +0330

    react-query was installed

--- my-app/src/components/Home.tsx
+++ my-app/src/components/Home.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { useAllCities } from "../hooks";
+
 
 interface Props {}
 
 const Home = (props: Props) => {
-  const { data, loading, error } = useAllCities();
+ 
 
-  if (loading) return <p>data is loading</p>;
 
-  const deleteCity:(id:number)=>void=(id)=>{
 
-  } 
+
+
+
+  
   return (
     <div className="container mt-5">
      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
@@ -25,8 +26,8 @@ const Home = (props: Props) => {
                   <h5 className="card-title text-start">name:{city.cityName}</h5>
                   <p className="card-text text-start">desc={city.description}</p>
                   <div className="d-flex justify-content-between mt-4">
-                      <button onClick={()=>deleteCity(city.id)} className="btn btn-outline-primary"><i className="bi bi-pencil-square"></i><span className="ms-2">Edit</span></button>
-                      <button className="btn btn-outline-danger"><i className="bi bi-trash"></i><span className="ms-2">Delete</span></button>
+                      <button  className="btn btn-outline-primary"><i className="bi bi-pencil-square"></i><span className="ms-2">Edit</span></button>
+                      <button onClick={()=>deleteCity(city.id)} className="btn btn-outline-danger"><i className="bi bi-trash"></i><span className="ms-2">Delete</span></button>
                   </div>
                 </div>
               </div>
--- my-app/src/hooks/index.ts
+++ my-app/src/hooks/index.ts
@@ -1,27 +1,34 @@
-import React,{useState,useEffect} from 'react';
-import * as api from "../api/index"
-import { city } from '../model/Types';
+import React, { useState, useEffect } from "react";
+import * as api from "../api/index";
+import { city } from "../model/Types";
+import {useQuery} from "react-query"
 
- type result={
-     data:city[] | null,
-     loading:boolean,
-     error:string
- }
+type result = {
+  data: city[] | null;
+  loading: boolean;
+  error: string;
+  setData:React.Dispatch<React.SetStateAction<city[] | null>>
+};
 
- export const useAllCities:() =>result  =()=>{
-    const [data, setData] = useState<null | city[]>(null)
-    const [loading, setLoading] = useState<boolean>(true)
-    const [error, setError] = useState<string>("")
+export const useAllCities: () => result = () => {
+ 
+    return useQuery("cities",()=>api.getAllCities())
 
-    useEffect(() => {
-        api.getAllCities().then(res=>{
-            console.log(res)
-            setData(res)
-            setLoading(false)
-        }).catch(error=>setError(error.response.message))
-     
-    }, [])
+  return { data, loading, error,setData };
+};
 
-    return {data,loading,error}
-}
 
+
+type deleteResult = [
+  fetch: (id?: number,data?:null | city[],setData?:React.Dispatch<React.SetStateAction<city[] | null>>) => void;
+];
+export const useDeleteCity: () => deleteResult = () => {
+
+  const fetch:(id:number,data:city[],setData:React.Dispatch<React.SetStateAction<city[] | null>>)=>void = (id,data,setData) => {
+    api.deleteCity(id).then(res=>{
+
+        setData(data.filter(item=>item.id !==id))
+    })
+  };
+  return [fetch] ;
+};
