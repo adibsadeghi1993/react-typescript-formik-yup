008cd4cc213d2259864fde73a6c691eb2d5fde61
adib sadeghi <adibsadeghi7272@gmail.com>
2021-12-25 17:08:42 +0330

    edit and delete btn was created to hendle edit and delete city

--- my-app/src/components/Home.tsx
+++ my-app/src/components/Home.tsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import { useAllCities } from '../hooks'
+import React from "react";
+import { useAllCities } from "../hooks";
 
-interface Props {
-    
-}
+interface Props {}
 
 const Home = (props: Props) => {
+  const { data, loading, error } = useAllCities();
 
-    const {data,loading,error}=useAllCities()
+  if (loading) return <p>data is loading</p>;
 
-    if(loading) return <p>data is loading</p>
-    return (
-        <div>
-         {data && data.map(city=>{
-             return <div key={city.id}>
-                 name:{city.cityName}
-                 <img src={city.image} alt="snan" />
-                 des:{city.description}
-                 </div>
-         })}   
-         <h1>home</h1>
-        </div>
-    )
-}
+  const deleteCity:(id:number)=>void=(id)=>{
 
-export default Home
+  } 
+  return (
+    <div className="container mt-5">
+     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
+    
+    {data &&
+        data.map((city) => {
+          return (
+            <div key={city.id}>
+              <div className="card">
+                <img src={city.image} className="card-img-top" alt="city" />
+                <div className="card-body">
+                  <h5 className="card-title text-start">name:{city.cityName}</h5>
+                  <p className="card-text text-start">desc={city.description}</p>
+                  <div className="d-flex justify-content-between mt-4">
+                      <button onClick={()=>deleteCity(city.id)} className="btn btn-outline-primary"><i className="bi bi-pencil-square"></i><span className="ms-2">Edit</span></button>
+                      <button className="btn btn-outline-danger"><i className="bi bi-trash"></i><span className="ms-2">Delete</span></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+     </div>
+  
+  );
+};
+
+export default Home;
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.render(
   <BrowserRouter>
