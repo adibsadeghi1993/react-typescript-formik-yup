c21557d9d012a055927018b17381b70f5d860d5c
adib sadeghi <adibsadeghi7272@gmail.com>
2021-12-25 19:23:46 +0330

    formik and yup was installed to handle form that was created by bootstrap

--- my-app/src/components/EditCity/EditCity.tsx
+++ my-app/src/components/EditCity/EditCity.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
-interface Props {
-    
-}
+interface Props {}
 
 const EditCity = (props: Props) => {
-   const {cityId}= useParams()
-   console.log(cityId)
-    return (
-        <div className='mt-5'>
-          <h1 className='mt-5'>hi city</h1>
-        </div>
-    )
-}
+  const { cityId } = useParams();
+  console.log(cityId);
+  return (
+    <div className="mt-3 row">
+      <div className="cols-12 cols-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <form>
+          <div className="mt-3">
+            <label className="form-label text-start w-100" htmlFor="cityName">
+              CityName
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="mt-3">
+            <label className="form-label text-start w-100" htmlFor="cityName">
+              ImageUrl
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="mt-3">
+            <label className="form-label text-start w-100">description</label>
+            <textarea
+              className="form-control text-start"
+              id="exampleFormControlTextarea1"
+            ></textarea>
+          </div>
+          <div className="mt-3">
+              <button className="btn btn-warning w-100">submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default EditCity
+export default EditCity;
